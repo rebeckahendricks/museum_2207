938a9a0ce1beb9018481ab9035d929cf97b9108e
Rebecka Hendricks <rebecka.hendricks@gmail.com>
2022-07-27 09:16:06 -0600

    feat/Create Exhibit class with attributes

--- spec/exhibit_spec.rb
+++ spec/exhibit_spec.rb
@@ -2,19 +2,19 @@ require './lib/exhibit'
 require './lib/patron'
 
 describe Exhibit do
-  xit 'is an instance of Exhibit' do
+  it 'is an instance of Exhibit' do
     exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
 
     expect(exhibit).to be_instance_of(Exhibit)
   end
 
-  xit 'has a name' do
+  it 'has a name' do
     exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
 
     expect(exhibit.name).to eq("Gems and Minerals")
   end
 
-  xit 'has a cost' do
+  it 'has a cost' do
     exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
 
     expect(exhibit.cost).to eq(0)
