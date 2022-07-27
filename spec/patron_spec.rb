810ab5f714981a8f7d3de22073491264b05382a0
Rebecka Hendricks <rebecka.hendricks@gmail.com>
2022-07-27 09:19:58 -0600

    feat/Create patron class, attributes, and add_interest method

--- spec/patron_spec.rb
+++ spec/patron_spec.rb
@@ -2,31 +2,31 @@ require './lib/exhibit'
 require './lib/patron'
 
 describe Patron do
-  xit 'is an instance of Patron' do
+  it 'is an instance of Patron' do
     patron_1 = Patron.new("Bob", 20)
 
     expect(patron_1).to be_instance_of(Patron)
   end
 
-  xit 'has a name' do
+  it 'has a name' do
     patron_1 = Patron.new("Bob", 20)
 
     expect(patron_1.name).to eq("Bob")
   end
 
-  xit 'it has spending money' do
+  it 'it has spending money' do
     patron_1 = Patron.new("Bob", 20)
 
     expect(patron_1.spending_money).to eq(20)
   end
 
-  xit 'it has no interests by default' do
+  it 'it has no interests by default' do
     patron_1 = Patron.new("Bob", 20)
 
     expect(patron_1.interests).to eq([])
   end
 
-  xit 'can add interests' do
+  it 'can add interests' do
     patron_1 = Patron.new("Bob", 20)
 
     patron_1.add_interest("Dead Sea Scrolls")
